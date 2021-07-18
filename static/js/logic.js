4e980c3cbdb331cb30aae71add082d573152e4da
Yazmin Rodriguez <79778345+Yazz-01@users.noreply.github.com>
2021-07-17 21:36:52 -0500

    corrected code

--- static/js/logic.js
+++ static/js/logic.js
@@ -65,79 +65,79 @@ d3.json(earthquakesURL).then(function(data) {
         //L.geoJson(data).addTo(myMap);
         //console.log(data)
     };
+});
 
-
-    //Define the marker color by depht for the eartqueakes----LISTO
-    function chooseColor(depth) {
-        switch (true) {
-            case depth > 90:
-                return "red";
-            case depth > 70:
-                return "orangered";
-            case depth > 50:
-                return "orange";
-            case depth > 30:
-                return "gold";
-            case depth > 10:
-                return "yellow";
-            default:
-                return "lightgreen";
-        }
+//Define the marker color by depht for the eartqueakes----LISTO
+function chooseColor(depth) {
+    switch (true) {
+        case depth > 90:
+            return "red";
+        case depth > 70:
+            return "orangered";
+        case depth > 50:
+            return "orange";
+        case depth > 30:
+            return "gold";
+        case depth > 10:
+            return "yellow";
+        default:
+            return "lightgreen";
     }
+}
 
-    // Create a GeoJSON layer containing the features array
-    // Each feature a popup describing the place and time of the earthquake
-    L.geoJSON(earthquakeData, {
-        pointToLayer: function(feature, latlng) {
-            return L.circleMarker(latlng,
-                // Set the style of the markers based on properties.mag
-                {
-                    radius: markerSize(feature.properties.mag),
-                    fillColor: chooseColor(feature.geometry.coordinates[2]),
-                    fillOpacity: 0.7,
-                    color: "black",
-                    stroke: true,
-                    weight: 0.5
-                }
-            );
-        },
-        onEachFeature: function(feature, layer) {
-            layer.bindPopup("<h3>Location: " + feature.properties.place + "</h3><hr><p>Date: " +
-                new Date(feature.properties.time) + "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
-        }
-    }).addTo(earthquakes);
-    // Sending our earthquakes layer to the createMap function
-    earthquakes.addTo(myMap);
+// Create a GeoJSON layer containing the features array
+// Each feature a popup describing the place and time of the earthquake
+L.geoJSON(earthquakeData, {
+    pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng,
+            // Set the style of the markers based on properties.mag
+            {
+                radius: markerSize(feature.properties.mag),
+                fillColor: chooseColor(feature.geometry.coordinates[2]),
+                fillOpacity: 0.7,
+                color: "black",
+                stroke: true,
+                weight: 0.5
+            }
+        );
+    },
+    onEachFeature: function(feature, layer) {
+        layer.bindPopup("<h3>Location: " + feature.properties.place + "</h3><hr><p>Date: " +
+            new Date(feature.properties.time) + "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
+    }
+}).addTo(earthquakes);
+// Sending our earthquakes layer to the createMap function
+earthquakes.addTo(myMap);
 
-    // Get the tectonic plate data from tectonicplatesURL
-    d3.json(tectonicplatesURL, function(data) {
-        L.geoJSON(data, {
-            color: "orange",
-            weight: 2
-        }).addTo(tectonicplates);
-        tectonicplates.addTo(myMap);
-    });
-    // Add legend
-    var legend = L.control({ position: "bottomright" });
-    legend.onAdd = function() {
-        var div = L.DomUtil.create("div", "info legend"),
-            depth = [-10, 10, 30, 50, 70, 90];
+// Get the tectonic plate data from tectonicplatesURL
+d3.json(tectonicplatesURL, function(data) {
+    L.geoJSON(data, {
+        color: "orange",
+        weight: 2
+    }).addTo(tectonicplates);
+    tectonicplates.addTo(myMap);
+});
+// Add legend
+var legend = L.control({ position: "bottomright" });
+legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend"),
+        depth = [-10, 10, 30, 50, 70, 90];
 
-        div.innerHTML += "<h3 style='text-align: center'>Depth</h3>"
+    div.innerHTML += "<h3 style='text-align: center'>Depth</h3>"
 
-        for (var i = 0; i < depth.length; i++) {
-            div.innerHTML +=
-                '<i style="background:' + chooseColor(depth[i] + 1) + '"></i> ' +
-                depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
-        }
-        return div;
-    };
-    legend.addTo(myMap);
+    for (var i = 0; i < depth.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + chooseColor(depth[i] + 1) + '"></i> ' +
+            depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+    }
+    return div;
+};
+legend.addTo(myMap)
 });
 // Llamar fucntion  deployMap --- 
-deployMap();
+deployMap()
 });
 
-/
+
 //Create a GeoJson layer containing the features array
 // Each feature
