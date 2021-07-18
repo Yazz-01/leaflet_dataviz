fd63d54bb003538c314627c2b6e2661436ef5ff5
Yazmin Rodriguez <79778345+Yazz-01@users.noreply.github.com>
2021-07-17 20:19:50 -0500

    updated geojson

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,34 +1,73 @@
 //Deine earthquakes and tectonic plates geoJson urls
-var earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson"
+var earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 var tectonicPlatesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 
 
 //Create two layerData
-var earthquakes = L.layerData();
-var tectonicPlates = L.layerData();
+var earthquakes = L.LayerGroup();
+var tectonicPlates = L.LayerGroup();
 
-//Define tile layers // Create map
-var satelliteMap = L.tileLayer(("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-            attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-            tileSize: 512,
-            maxZoom: 15
-            id: "mapbox.satellite"
-        });
+function deployMap() {
+    //Define tile layers // Create map
+    var satelliteMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox.satellite",
+        accessToken: API_KEY
+    });
 
-        // Adding a tile layer (the background map image) to our map
-        // We use the addTo method to add objects to our map
-        L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-            attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-            tileSize: 512,
-            maxZoom: 18,
-            zoomOffset: -1,
-            id: "mapbox/streets-v11",
-            accessToken: API_KEY
-        }).addTo(myMap);
+    // Adding a tile layer (the background map image) to our map
+    // We use the addTo method to add objects to our map
+    var streestmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
+    });
 
-        //Data - Promise
-        var link = "static/data/eartquak.geojson";
+    // Define a baseMaps object to hold our base layers
+    var baseMaps = {
+        "Street Map": streetmap,
+        "Satellite Map": satelliteMap
+    };
+    // Create overlay object to hold our overlay layer
+    var overlayMaps = {
+        Earthquakes: earthquakes,
+        TectonicPlates: tectonicPlates
+    };
 
-        d3.json(link).then(function(data) {
-            L.geoJson(data).addTo(myMap);
-        });
+    // Create our map, giving it the streetmap and earthquakes layers to display on load
+    var myMap = L.map("map", {
+        center: [
+            37.09, -95.71
+        ],
+        zoom: 5
+    });
+    // Create a layer control
+    // Pass in our baseMaps and overlayMaps
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
+}
+
+
+//Data - Promise
+
+d3.json(earthquakesURL).then(function(data) {
+    //L.geoJson(data).addTo(myMap);
+    console.log(data)
+        //Extraer los terremotos
+
+    // Markers para los terremotos magnitud
+
+    // Llamar fucntion  deployMap
+    deployMap();
+
+
+
+});
